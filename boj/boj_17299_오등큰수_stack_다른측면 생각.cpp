c5ed93b99d9499179729ed705aeb0eda31357829
elyo9381 <won4413@daum.net>
2020-01-06 02:10:38 +0900

    studied compare stack index and vector reverse index '오큰수'

--- boj/boj_17299_오등큰수_stack_다른측면 생각.cpp
+++ boj/boj_17299_오등큰수_stack_다른측면 생각.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-
 using namespace std;
 
 /*오큰수 다른 측면에서 생각하였을때 가장 오른쪽 n이 n-1보다
-크면 n을 출력하고 n이 n-1작으면 스택을 pop하면 되는 구조이다. */
+크면 n을 출력하고 n이 n-1작으면 스택을 pop하면 되는 구조이다. 
+비교하여 풀기 
+*/
 int test_case;
 
 int temp;
@@ -13,6 +14,8 @@ int temp;
 vector<int> v;
 vector<int> ans;
 stack<int> s;
+
+int count_num[1000001] = { 0 };
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -22,27 +25,34 @@ int main(void) {
     for (int i = 0; i < test_case; i++) {
         cin >> temp;
         v.push_back(temp);
+        count_num[temp]++;
     }
 
     s.push(-1);
 
-    for (int i = v.size() - 1; i >= 0; i--) {
-        if (s.top() <= v[i]) {  //스택의 맨 위가 백터보다 더 크면
-            while (s.top() <= v[i] && s.top() != -1)
+    for (int i = 0; i <= v.size() - 1; i++) {
+        cout<<"<1>"<<endl;
+        if (count_num[s.top()] <= count_num[v[i]]) {  //스택맨위가 백터보더 더 작으면
+            while (count_num[s.top()] <= count_num[v[i]] && s.top() != -1){
+                cout<<"<3>"<<endl;
                 s.pop();
+            }
             ans.push_back(s.top());
+            cout<<"<4>"<<endl;
             s.push(v[i]);
+            cout<<"<5>"<<endl;
         }
         else {
+            cout<<"<6>"<<endl;
             ans.push_back(s.top());
+            cout<<"<7>"<<endl;
             s.push(v[i]);
         }
     }
 
-    for (int i = ans.size() - 1; i >= 0; i--) {
+     for (int i = 0; i <= v.size() - 1; i++)  {
         cout << ans[i] << " ";
     }
 
-    system("pause");
     return 0;
 }
