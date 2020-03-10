3edf4907e89d8cfa089973cbf1616d7d617937d8
elyo9381 <won4413@daum.net>
2020-03-10 22:32:37 +0900

    studied simulation & deque

---
+++ boj/boj_11559_anypang_구현_DFS&BFS.cpp
@@ -0,0 +1,280 @@
+#include <bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+#define endl '\n'
+#define MAXN 1001
+#define pf1l(a) cout << a << '\n'
+#define pf1(a) cout << a << ' '
+#define sf1(a) cin >> a
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define sf2(a,b) cin >> a >> b
+
+using namespace std;
+
+/*
+    
+*/
+
+
+char MAP[12][6];
+bool visit[12][6];
+
+int boom_cnt = 0;
+int temp_cnt = 0;
+
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+
+vector<pair<int, int> > Boom_tmp, Boom_vec;
+
+void Input()
+{
+    for(int i  = 0; i<12; i++)
+        for(int j =0; j<6; j++)
+            cin >> MAP[i][j];
+}
+
+void DFS(int x, int y)
+{
+    for(int i =  0; i< 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 || ny < 0 || nx >= 12 || ny >= 6) continue; //범위를 넘어갔다면
+        if(MAP[nx][ny] == '.') continue; // 빈공간이라면 
+        if(visit[nx][ny] == true) continue; // 방문하지 않았다면
+        if(MAP[x][y] != MAP[nx][ny] ) continue; // 같은 색상이 아니면
+
+        // 같은색상이면 같은색임의 cnt를 올린다.
+        // 그리고  방문여부를 트루로한다.
+        // Boom_tmp에는 같은 색상이 4개이상되면 터트려야하므로
+        // 4개 이상인지 확인하기 위해서 Boom_tmp에 넣는다.  
+        temp_cnt++; 
+        visit[nx][ny] = true;
+        Boom_tmp.push_back(make_pair(nx,ny));
+        DFS(nx,ny);
+    }
+}
+void Solution()
+{  
+   bool Flag;
+   int Answer = 0;
+
+   while(1)
+   {
+        Flag = false;
+        memset(visit, false, sizeof(visit));
+        Boom_vec.clear();
+
+        for(int i = 0; i<12; i++)
+        {
+            for(int j =0; j< 6; j++)
+            {
+                if(MAP[i][j] != '.' && visit[i][j] == false)
+                {
+                    temp_cnt = 1;
+                    Boom_tmp.push_back(make_pair(i,j));
+                    visit[i][j] = true;
+                    DFS(i,j);
+
+                    if(temp_cnt >= 4)
+                    {
+                        Flag =  true;
+
+                        for(int i =0; i<Boom_tmp.size(); i++)
+                        {
+                            //Boom_vec는 4개이상된 색상을 터트리기 위해서 
+                            // 다시 다른 배열에 넣고 Boom_tmp는 또 다른 색상을 찾기위해서
+                            // 초기화 한다. 
+                            Boom_vec.push_back(Boom_tmp[i]);
+                        }
+                    }
+                    Boom_tmp.clear();
+                }
+            }
+        }
+
+        // 색이 있던곳에는 빈공간으로 표기하고 
+        for(int i = 0; i<Boom_vec.size(); i++)
+        {
+            int x = Boom_vec[i].first;
+            int y = Boom_vec[i].second;
+
+            MAP[x][y] = '.';
+        }
+
+        // 아래서부터 정렬하면된다. 
+        // 색상이 있을때 하나식 while(1)을통해서 하나식 내리면 된다.
+        for(int i= 10; i>=0; i--)
+        {
+            for(int j = 0; j<6 ;j++)
+            {
+                if(MAP[i][j] =='.') continue;
+                int tmp = i;
+
+            while(1)
+            {
+                if(tmp == 11 || MAP[tmp+1][j] != '.') break;
+                MAP[tmp+1][j] = MAP[tmp][j];
+                MAP[tmp][j] = '.';
+                tmp++;
+            }
+        }
+   }
+
+   if(Flag == true) Answer++;
+   else break;
+}
+
+cout << Answer << endl;
+
+            }
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solve();
+    return 0;
+}
+
+
+char MAP[12][6];
+bool visit[12][6];
+
+
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+
+
+void Input()
+{
+    for(int i = 0; i<12; i++)
+        scanf("%s",MAP[i]);
+}
+
+vector<pii> getColor(char color)
+{
+    vector<pii> vc;
+    for(int i = 0; i <12; i++)
+        for(int j = 0; j<6; j++)
+            if( MAP[i][j] == color)
+                vc.push_back({i,j});
+
+    return vc;
+}
+
+bool bfs(char color, vector<pii> vc)
+{
+    queue<pii> q;
+    bool chk =  false;
+
+    for( int i =0; i< vc.size(); i++)
+    {
+        int x = vc[i].first;
+        int y = vc[i].second;
+
+        q.push({x,y});
+        int cnt = 0;
+        memset(visit, 0, sizeof(visit));
+
+        while(!q.empty())
+        {
+            int hx = q.front().first;
+            int hy = q.front().second;
+
+            q.pop();
+
+            if(visit[hx][hy])
+                continue;
+
+            visit[hx][hy] = true;
+            cnt++;
+            for( int j = 0; j < 4; j++)
+            {
+                int ny = hy + dy[j];
+                int nx = hx + dx[j];
+
+           if ((0 <= ny && ny < 12 && (0 <= nx && nx < 12)) && MAP[nx][ny] == color)
+                    q.push({ nx,ny });
+
+            }
+        }
+
+        if( cnt >= 4)
+        {
+            for(int i = 0; i< 12; i++)
+                for(int j =0; j< 6 ; j++)
+                    if(visit[i][j])
+                    {
+                        MAP[i][j] = '.';
+                        chk= true;
+                    }
+        }
+    }
+
+    if(chk)
+        return true;
+    else
+        return false;
+}
+
+void Solution()
+{  
+  int combo = 0;
+
+  while(1)
+  {
+    bool isEvent = false;
+
+    vector<pii> vcR = getColor('R');
+    vector<pii> vcG = getColor('G');
+    vector<pii> vcB = getColor('B');
+    vector<pii> vcP = getColor('P');
+    vector<pii> vcY = getColor('Y');
+
+    bool r = bfs('R', vcR);
+    bool g = bfs('G', vcG);
+    bool b = bfs('B', vcB);
+    bool p = bfs('P', vcP);
+    bool y = bfs('Y', vcY);
+
+    if(r || g || b || p || y)
+        isEvent = true;
+
+    if(isEvent)
+        combo++;
+    else
+        break;
+
+    //재정렬
+    for(int t = 0; t < 12; t++)
+        for(int i =0; i<12; i++)
+        {
+            for( int j = 0; j<6; j++)
+            {
+                if( MAP[i][j] != '.' && MAP[i+1][j] =='.')
+                {
+                    MAP[i+1][j] = MAP[i][j];
+                    MAP[i][j] = '.';
+                }
+            }
+        }
+  }
+  cout << combo <<endl;
+}
---
+++ boj/boj_14891_wheel_deque_simulation.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+#define endl '\n'
+#define MAXN 1001
+#define pf1l(a) cout << a << '\n'
+#define pf1(a) cout << a << ' '
+#define sf1(a) cin >> a
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define sf2(a,b) cin >> a >> b
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+/*
+    초기 입력은 스트링을 통해서 입력받았다 그리고 '0'을 빼줘서 
+    int값으로 변형하였는데 
+    처음부터 "1","0"을 int형으로 받아도 될거같다.
+
+    pair를 사용하여 몇번째 바퀴이고 
+    반시계로 가야하는지 시계로 가야하는지 체크하였다.
+
+    조건에서 같은극일때는 움직임이없고 
+    다른극일때는 반대반향으로 움직이는것은 방향 *= -1;을 통해서 구현하였다.
+
+    deque를 이용해서 시계방향과 반시계반향의 
+    입력과 삭제를 실행하고 바퀴의 순서는 q를 이용하여서 입력을 받았다. 
+    
+*/
+
+deque<int> dp[5];
+int k; 
+
+void Input()
+{
+    for(int i =1; i<5; i++)
+    {
+        string s;
+        cin >> s;
+
+        for(int j=0; j<s.length(); j++)
+            dq[i].push_back(s[j] - '0');
+    }
+
+}
+
+void Solution()
+{  
+    cin >> k;   
+
+    for(int i = 0; i< k; i++)
+    {
+        int num, dir;
+        cin >> num >> dir;
+
+        int idx = num;
+        int tempDir = dir;
+        queue<pair<int, int>> q;
+        q.push({ idx, tempDir });
+
+        while(1)
+        {
+            if(idx == 4)
+                break;
+
+            idx++;
+            tempDir *= -1;
+            if( dq[idx-1][2] != dq[idx][6])
+                q.push({idx, tempDir});
+            else 
+                break;
+        }
+
+        idx = num;
+        tempDir = dir;
+        while(1)
+        {
+            if(idx ==1)
+                break;
+
+            idx--;
+            tempDir *= -1;
+            if( dq[idx +1][6] != dq[idx][2])
+                q.push({idx, tempDir});
+            else
+                break;
+        }
+
+        while(!q.empty())
+        {
+            int cur = q.front().first;
+            int rotate = q.front().second;
+            q.pop();
+
+            if(rotate == 1)
+            {
+                int temp = dq[cur].back();
+                dq[cur].pop_back();
+                dq[cur].pop_front(temp);
+            }
+            else
+            {
+                int temp = dp[cur].front();
+                dq[cur].pop_front();
+                dq[cur].pop_back(temp);
+            }
+        }
+    }
+
+    int result = 0;
+
+    for (int i = 1; i < 5; i++)
+             if (dq[i].front() == 1)
+                     result += (int)pow(2, i - 1);
+
+    cout << result << "\n";
+}
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solve();
+    return 0;
+}
