464fb9def3eb8bc50bbc777881531507f74c1e5f
elyo9381 <won4413@daum.net>
2020-03-24 23:30:32 +0900

    studied bfs

--- boj/boj_11403_findPath_DFS&BFS(with cycle).cpp
+++ boj/boj_11403_findPath_DFS&BFS(with cycle).cpp
@@ -55,7 +55,7 @@ void dfs(int point, int x, int y)
 
 	for(int  i= 0; i <N; i++)
 	{
-		if(dvis[point][i] == false && MAP[y][i] ==1)
+		if(dvis[point][i] == false && MAP[y][i] == 1)
 			dfs(point,y,i);
 	}
 
@@ -107,3 +107,49 @@ int main(void)
 }
 
 
+// void dfs(int x, int y)
+// {
+//     if(!rchk[idx][y]) rchk[idx][y] = true;
+//     if(chk[x][y]) chk[x][y] = true;
+//     for(int i = 0; i<v[x].size(); i++)
+//     {
+//         if(chk[x][v[y][i]]) continue;
+//         dfs(y,v[y][i]]);
+//     } 
+// }
+
+// 아하 그래프도 인접행렬 식으로 받아올수있구나 
+// 조건물을 사용하여서 조건에 해당하는 글자 혹은 문자일때 
+// 이중포문을 설정하여 i번째에 j가 연결된 형태로 표현이 
+// 가능하다는걸 알게 되었다. 
+
+// 그리고 
+
+// dfs에서 인덱스를 저런식으로 받고 나서 chk[x][v[y][i]]
+// 식으로 인덱스를 받는다면 2차원배열에서 i,j검사중에
+// j에 해당하는 인덱스의 (j+1,k)의검사가 가능하겠구나
+// 사이클 형식의 review가 가능한것이었어 그렇구나
+
+// scanf("%d", &n);
+
+// memset
+
+// for(i,1,n+1)
+// {
+//     for(j,1,n+1)
+//     {
+//         scanf("%d", &d);
+//         if(d==1) v[i].push_back(j);
+//     }
+// }
+
+
+// for(i,1,n+1)
+// {
+//     for(j,1,n+1)
+//     {
+//         memset(chk,false, sizeof(chk));
+//         idx= i;
+//         dfs(i, v[i][j]);
+//     }
+// }
