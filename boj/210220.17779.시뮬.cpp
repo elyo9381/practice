e3fac3710b7775d3cc3b1a4666c34f612a3cf102
elyo9381 <won4413@daum.net>
2021-02-21 23:35:42 +0900

    solve simul

--- boj/210220.17779.시뮬.cpp
+++ boj/210220.17779.시뮬.cpp
@@ -26,18 +26,22 @@ int solve(int x ,int y,int d1, int d2){
     tmp[x][y] = 5;
 
     // 경계선 그려 넣기 
+    // (x,y), (x+1,y-1), ...., (x+d1,y-d1)
+    // (x + d2 , y + d2), ...., (x+d2+d1, y+ d2 -d1)
     for(int i = 1; i<=d1; i++){
         tmp[x + i][y - i] = 5;
-        tmp[x + d2 + i][x + d2 - i] = 5;
+        tmp[x + d2 + i][y + d2 - i] = 5;
     }
+
+    // (x + d1 , y - d1), ...., (x + d1 + d2, y-d1 +d2)
     for(int i = 1; i<=d2 ; i++){
         tmp[x+i][y+i] = 5;
         tmp[x+d1+i][y-d1+i] = 5;
     }
 
-    // 1선거구
+    // 1선거구 : 1 ≤ r < x+d1, 1 ≤ c ≤ y
     int aa = 0;
-    for(int r = 1; r < x +d1; r++){
+    for(int r = 1; r < x +d1; ++r){
         for(int c =1; c<=y; ++c){
             if(tmp[r][c] == 5){
                 break;
@@ -46,10 +50,10 @@ int solve(int x ,int y,int d1, int d2){
         }
     }
 
-    // 2선거구
+    // 2선거구 : 1 ≤ r ≤ x+d2, y < c ≤ N
     int bb = 0;
-    for(int r = 1 ; r <= x+d2 ; r++){
-        for(int c = n; c > y; c--){
+    for(int r = 1 ; r <= x+d2 ; ++r){
+        for(int c = n; c > y; --c){
             if(tmp[r][c] == 5){
                 break;
             }
@@ -57,10 +61,10 @@ int solve(int x ,int y,int d1, int d2){
         }
     }
 
-    // 3선거구
+    // 3선거구 : x+d1 ≤ r ≤ N, 1 ≤ c < y-d1+d2
     int cc = 0;
     for(int r = x+d1 ; r<=n ; ++r){
-        for(int c = 1; c< y - d1 ; ++c){
+        for(int c = 1; c < y - d1 +d2 ; ++c){
             if(tmp[r][c] == 5){
                 break;
             }
@@ -68,10 +72,10 @@ int solve(int x ,int y,int d1, int d2){
         }
     }
 
-    // 4선거구
+    // 4번 선거구: x+d2 < r ≤ N, y-d1+d2 ≤ c ≤ N
     int dd = 0;
-    for(int r = x + d2 + 1; r<=n; r++){
-        for(int c = n; c>=y - d1 +d2; --c){
+    for(int r = x + d2 + 1; r<=n; ++r){
+        for(int c = n; c >= y - d1 +d2; --c){
             if(tmp[r][c] == 5){
                 break;
             }
@@ -79,11 +83,11 @@ int solve(int x ,int y,int d1, int d2){
         }
     }
 
-    int ee = sum - (aa+ bb +cc +dd);
+    int ee = sum - aa - bb -cc - dd;
     
     int maxValue = max(aa,max(bb,max(cc,max(dd,ee))));
     int minValue = min(aa,min(bb,min(cc,min(dd,ee))));
-    return maxValue - minValue;
+    return (maxValue - minValue);
 }
 
 
@@ -98,13 +102,13 @@ int main(){
         }
     }
     
-    int ret = 987654321;
+    int ret = 0x7fffffff;
     for(int x = 1; x<=n; x++){
         for(int y = 1; y<=n; y++){
             for(int d1 = 1; d1 <= n; d1++){
                 for(int d2 = 1; d2 <=n ; d2++){
-                    if(x + d1 + d2 >n) continue;
-                    if(1 > y -d1) continue;
+                    if(x + d1 + d2 > n) continue;
+                    if(1 > y - d1) continue;
                     if(y + d2 > n) continue;
                     ret = min(ret,solve(x,y,d1,d2));
                 }
