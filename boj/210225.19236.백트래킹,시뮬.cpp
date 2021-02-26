def31f4169dfaed5eb2ba635f78dc83318cf67eb
elyo9381 <won4413@daum.net>
2021-02-26 23:50:39 +0900

    solve simul

--- boj/210225.19236.백트래킹,시뮬.cpp
+++ boj/210225.19236.백트래킹,시뮬.cpp
@@ -1,109 +1,130 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
 
-struct FISH {
-	int y, x;
-	int dir;
-};
-
-const int dy[8] = { -1, -1, 0, +1, +1, +1, 0, -1 };
-const int dx[8] = { 0, -1, -1, -1, 0, +1, +1, +1 };
+using namespace std;
 
 int ret;
 
-void solve(int board[4][4], FISH fish[], int shark_y, int shark_x, int sum) {
-	int candi_board[4][4];
-	FISH candi_fish[16];
-	for (int y = 0; y < 4; ++y) {
-		for (int x = 0; x < 4; ++x) {
-			candi_board[y][x] = board[y][x];
-		}
-	}
-	for (int i = 0; i < 16; ++i) {
-		candi_fish[i] = fish[i];
-	}
+struct FISH {
+    int y , x;
+    int dir;
+};
 
-	//eat
-	int fish_number = candi_board[shark_y][shark_x];
-	int shark_dir = candi_fish[fish_number].dir;
-	candi_fish[fish_number].y = -1;
-	candi_fish[fish_number].x = -1;
-	candi_fish[fish_number].dir = -1;
-	candi_board[shark_y][shark_x] = -1;
+const int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dx[8] = { 0, -1, -1, -1, 0 , 1, 1, 1};
 
-	sum += (fish_number + 1);
-	if (ret < sum) {
-		ret = sum;
-	}
+void solve(int board[4][4], FISH fish[], int shark_y, int  shark_x, int sum){
+    int candiBoard[4][4];
+    FISH candiFish[16];
+    for(int y = 0; y < 4; y++){
+        for(int x = 0; x < 4; x++){
+            candiBoard[y][x] = board[y][x];
+        }
+    }
 
-	//fish move
-	for (int f = 0; f < 16; ++f) {
-		if (candi_fish[f].y == -1) {
-			continue;
-		}
-		int cy = candi_fish[f].y;
-		int cx = candi_fish[f].x;
-		int cd = candi_fish[f].dir;
+    for(int i = 0; i < 16; i++){
+        candiFish[i] = fish[i];
+    }
 
-		int ny = cy + dy[cd];
-		int nx = cx + dx[cd];
-		int nd = cd;
-		while (ny < 0 || ny >= 4 || nx < 0 || nx >= 4 || (ny == shark_y && nx == shark_x)) {
-			nd = (nd + 1) % 8;
-			ny = cy + dy[nd];
-			nx = cx + dx[nd];
-		}
+    //eat
+    int fishNumber = candiBoard[shark_y][shark_x];
+    int sharkDir = candiFish[fishNumber].dir;
+    candiFish[fishNumber].y = -1;
+    candiFish[fishNumber].x = -1;
+    candiFish[fishNumber].dir = -1;
+    candiBoard[shark_y][shark_x] = -1;
 
-		if (candi_board[ny][nx] != -1) {
-			int target = candi_board[ny][nx];
-			candi_fish[target].y = cy;
-			candi_fish[target].x = cx;
-			candi_fish[f].y = ny;
-			candi_fish[f].x = nx;
-			candi_fish[f].dir = nd;
+    sum += (fishNumber +1);
+    if(ret < sum) {
+        ret = sum;
+    }
 
-			candi_board[ny][nx] = f;
-			candi_board[cy][cx] = target;
-		}
-		else {
-			candi_fish[f].y = ny;
-			candi_fish[f].x = nx;
-			candi_fish[f].dir = nd;
+    //fish move
+    for(int f = 0; f < 16; ++f){
+        if(candiFish[f].y == -1) {
+            continue;
+        }
 
-			candi_board[ny][nx] = f;
-			candi_board[cy][cx] = -1;
-		}
-	}
+        int cy = candiFish[f].y;
+        int cx = candiFish[f].x;
+        int cd = candiFish[f].dir;
 
-	//shark move
-	for (int step = 1; step < 4; ++step) {
-		int ny = shark_y + dy[shark_dir] * step;
-		int nx = shark_x + dx[shark_dir] * step;
-		if (ny < 0 || ny >= 4 || nx < 0 || nx >= 4) {
-			break;
-		}
-		if (candi_board[ny][nx] != -1) {
-			solve(candi_board, candi_fish, ny, nx, sum);
-		}
-	}
+        int ny = cy + dy[cd];
+        int nx = cx + dx[cd];
+        int nd = cd;
+
+        // 범위 벗어났을때 왼쪽 45도 rotation
+        while( ny < 0 || ny >= 4 || nx < 0 | nx >=4 || (ny == shark_y && nx == shark_x)) {
+            nd = (nd + 1) % 8;
+            ny = cy + dy[nd];
+            nx = cx + dx[nd];
+        }
+
+        // !(상어를 만났을때 && 빈칸을 만남)
+        if(candiBoard[ny][nx] != -1){
+            int target = candiBoard[ny][nx];
+            candiFish[target].y = cy;
+            candiFish[target].x = cx;
+            candiFish[f].y = ny;
+            candiFish[f].x = nx;
+            candiFish[f].dir = nd;
+
+            candiBoard[ny][nx] = f;
+            candiBoard[cy][cx] = target;
+        } 
+        else { // 빈칸을 만나면 빈칸에 물고기를 이동시킨다.
+            candiFish[f].y = ny;
+            candiFish[f].x = nx;
+            candiFish[f].dir = nd;
+
+            candiBoard[ny][nx] = f;
+            candiBoard[cy][cx] = -1;
+        }
+    }
+
+    // shark move 
+    for(int step = 1; step < 4; ++step){
+        int ny = shark_y + dy[sharkDir] * step;
+        int nx = shark_x + dx[sharkDir] * step;
+        if(ny < 0 || ny >=4 || nx < 0 || nx >=4) {
+            break;
+        }
+        if(candiBoard[nx][ny] != -1) {
+            solve(candiBoard, candiFish,ny,nx,sum);
+        }
+    }
+
 }
 
-int main()
-{
-	FISH fish[16];
-	int board[4][4];
-	for (int y = 0; y < 4; ++y) {
-		for (int x = 0; x < 4; ++x) {
-			int a, b;
-			scanf("%d %d", &a, &b);
-			--a, --b;
-			fish[a].y = y;
-			fish[a].x = x;
-			fish[a].dir = b;
-			board[y][x] = a;
-		}
-	}
-	ret = 0;
-	solve(board, fish, 0, 0, 0);
-	printf("%d\n", ret);
-	return 0;
+
+int main(){
+
+    // 인덱스를 활용한 보드와 객체의 정보를 내포하는 fish 배열이 존재한다. 
+    FISH fish[16];
+    int board[4][4];
+
+    for(int y = 0; y<4; y++){
+        for(int x = 0; x< 4; ++x){
+            int a, b;
+            cin >> a >> b;
+            --a, --b;
+            fish[a].y = y;
+            fish[a].x = x;
+            fish[a].dir = b;
+            board[y][x] = a;
+        }
+    }
+
+    ret = 0;
+    solve(board, fish, 0, 0, 0);
+    cout << ret << "\n";
+
+
+   
 }
