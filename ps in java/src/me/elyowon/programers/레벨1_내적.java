8ace226fc12533403f66ebc08c0dbf1751f33928
elyo9381 <won4413@daum.net>
2021-06-20 22:34:01 +0900

    study programmers 폰켓몬, 체육복

---
+++ ps in java/src/me/elyowon/programers/레벨1_내적.java
@@ -0,0 +1,23 @@
+package me.elyowon.programers;
+
+
+/**
+ * 2021.06.20
+ */
+public class 레벨1_내적 {
+
+
+    public int solution(int[] a, int[] b) {
+
+        int answer = 0;
+
+
+        int len = a.length;
+        while(len > 0){
+            long sum = a[len] * b[len];
+
+            answer +=sum;
+        }
+        return answer;
+    }
+}
--- ps in java/src/me/elyowon/programers/레벨1_키패드누르기.java
+++ ps in java/src/me/elyowon/programers/레벨1_키패드누르기.java
@@ -15,55 +15,53 @@ public class 레벨1_키패드누르기 {
 
         System.out.println("s = " + s);
     }
+    public static String solution(int[] numbers, String hand) {
+        StringBuilder sb = new StringBuilder();
+        int leftIndex = 10;	 // '*' => 10으로 치환
+        int rightIndex = 12; // '#' => 12로 치환
 
-    public static String solution(int[] numbers,String hand) {
-        String answer = "";
-        int leftIndex = 10;
-        int rightIndex = 12;
-
-        for (int number : numbers) {
-            if (number == 1 || number == 4 || number == 7) {
-                answer += "L";
-
+        for(int number : numbers) {
+            if(number == 1 || number == 4 || number == 7) { // Left
+                sb.append("L");
                 leftIndex = number;
-            } else if (number == 3 || number == 6 || number == 9) {
-                answer += "R";
-
+            } else if(number == 3 || number == 6 || number == 9) {	// Right
+                sb.append("R");
                 rightIndex = number;
-            } else {
-                int leftLength = getLength(leftIndex,number);
-                int rightLength = getLength(rightIndex,number);
+            } else { // Center
+                int leftLength = getLength(leftIndex, number);
+                int rightLength = getLength(rightIndex, number);
 
-                if (leftLength > rightLength) {
-                    answer += "R";
+                if(leftLength > rightLength) {
+                    sb.append("R");
                     rightIndex = number;
-                } else if (leftLength < rightLength) {
-                    answer += "L";
+                } else if(leftLength < rightLength) {
+                    sb.append("L");
                     leftIndex = number;
                 } else {
-                    if (hand.equals("right")) {
-                        answer += "R";
+                    if(hand.equals("right")) {
+                        sb.append("R");
                         rightIndex = number;
                     } else {
-                        answer += "L";
+                        sb.append("L");
                         leftIndex = number;
                     }
                 }
             }
         }
-        return answer;
+        return sb.toString();
     }
 
-    public static int getLength(int index,int number) {
+    public static int getLength(int index, int number) {
 
-            index = (index == 0) ? 11 : index;
-            number = (number == 0) ? 11 : number;
+        // 숫자 0의 경우 11로 치환
+        index = (index == 0) ? 11 : index;
+        number = (number == 0) ? 11 : number;
 
-            int x = (index - 1) / 3;
-            int y = (index - 1) % 3;
-            int numberX = number / 3;
-            int numberY = 1;
+        int x = (index - 1) / 3;
+        int y = (index - 1) % 3;
+        int numberX = number / 3;
+        int numberY = 1;
 
-            return Math.abs(x-numberX) + Math.abs(y-numberY);
-        }
+        return Math.abs(x - numberX) + Math.abs(y - numberY);
+    }
 }
